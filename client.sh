8e3525cc21878d1afd569830321b8327c3676753
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2024-04-23 15:52:58 -0300

    Update triton version

--- client.sh
+++ client.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -it --rm --gpus=all --net=host -v ./tmp/triton_repo:/mnt --name triton-client nvcr.io/nvidia/tritonserver:23.09-py3-sdk
+docker run -it --rm --gpus=all --net=host -v ./tmp/triton_repo:/mnt --name triton-client nvcr.io/nvidia/tritonserver:21.10-py3-sdk
