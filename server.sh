f859e028b96345edd2a2cdc1ac7e3fd215f30e41
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2024-04-25 14:22:08 +0000

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -it --rm -p 8000:8000 -p 8001:8001 -p 8002:8002 --gpus all -v ./tmp/triton_repo:/mnt -v ./mds:/models --name triton-server nvcr.io/nvidia/tritonserver:21.10-py3
+docker run -it --rm -p 8000:8000 -p 8001:8001 -p 8002:8002 --gpus all -v ./tmp/triton_repo:/mnt -v ./mds:/models --name triton-server nvcr.io/nvidia/tritonserver:23.09-py3
